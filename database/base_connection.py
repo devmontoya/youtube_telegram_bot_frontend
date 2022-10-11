20f423d92a0bb5f47e6307ed6ac9ef0888cb4908
Daniel Montoya <da03@protonmail.com>
2022-10-10 19:32:08 -0500

    Setup project: pre-commit, poetry

---
+++ database/base_connection.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from config import settings
+
+engine = create_engine(settings.engine_db)
+Session = sessionmaker(bind=engine)
+
+Base = declarative_base()
